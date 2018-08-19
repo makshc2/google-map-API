af833ab606ff6862d48b4a41976b0117d24d97d0
Maksim Shevjakov <makshc2@gmail.com>
2018-08-19 17:04:00 +0300

    git add maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+    agm-map {
+      height: 100vh;
+    }
+  `],
+  template: `
+  <agm-map [latitude]="lat" [longitude]="lng"></agm-map>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  lat = 51.678418;
+  lng = 7.809007;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { MapsComponent } from './components/maps/maps.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBkW-bXqM76CnUgvYdQfZ7Kd6NotX82ut4'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
